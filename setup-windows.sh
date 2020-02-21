6830e6b9a7a0ca62aefe223a19fad3a679c38a5e
gmierz <gmierz1@live.ca>
2020-02-21 14:41:38 -0500

    Add ffmpeg to PATH.

--- setup-windows.sh
+++ setup-windows.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env sh
+set -x
 
 choco install curl
 choco install unzip
 
 curl -o ffmpeg.zip https://github.com/ncalexan/geckodriver/releases/download/v0.24.0-android/ffmpeg-4.1.1-macos64-static.zip
 
-unzip ffmpeg.zip -d ffmpeg
+unzip ffmpeg.zip -d $TRAVIS_BUILD_DIR/ffmpeg
 
 echo "SUCCESS 1"
 
-ffmpeg/bin/ffmpeg.exe
+$TRAVIS_BUILD_DIR/ffmpeg/bin/ffmpeg.exe
 
 echo "SUCCESS 2"
 
-ffmpeg/bin/ffmpeg.exe --help
+$TRAVIS_BUILD_DIR/ffmpeg/bin/ffmpeg.exe --help
 
 echo "SUCCESS 3"
+
+export PATH="$TRAVIS_BUILD_DIR/ffmpeg/bin/:$PATH"
