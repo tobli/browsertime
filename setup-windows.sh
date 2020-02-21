fa203f1a5984c88f0bdeff1189e36d2a8f80f61b
gmierz <gmierz1@live.ca>
2020-02-21 14:31:46 -0500

    Use script for testing and disable installs we don't need now.

---
+++ setup-windows.sh
@@ -0,0 +1,17 @@
+
+choco install curl
+choco install unzip
+
+curl -o ffmpeg.zip https://github.com/ncalexan/geckodriver/releases/download/v0.24.0-android/ffmpeg-4.1.1-macos64-static.zip
+
+unzip ffmpeg.zip -d ffmpeg
+
+echo "SUCCESS 1"
+
+ffmpeg/bin/ffmpeg.exe
+
+echo "SUCCESS 2"
+
+ffmpeg/bin/ffmpeg.exe --help
+
+echo "SUCCESS 3"
